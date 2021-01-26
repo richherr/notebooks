fcf3782b902e54f6a71e87f658866d42b83299d2
richherr <richherr@gmail.com>
2021-01-26 11:06:31 -0600

    .

---
+++ install_packages.R
@@ -0,0 +1,10 @@
+install.packages("gridExtra") # Download and install gridExtra
+install.packages("hexbin") # Download and install hexbin
+install.packages("tidyverse") # Download and install tidyverse
+install.packages("lubridate")
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("magrittr")
+install.packages("tidyr")
+install.packages("knitr")
